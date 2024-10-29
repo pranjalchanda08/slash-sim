c6e4d273a0e7fa1997e487ab2d7681a93748225b
Pranjal Chanda <pranjalchanda08@gmail.com>
2024-10-29 23:11:00 +0530

    added rv32_err_t for error manangement inc directory revamped

---
+++ inc/common/rv32_err.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef enum
+{
+    RV32_SUCCESS,
+    RV32_ERR_ASSERT_PARAM,
+    RV32_ERR_RAM_INIT,
+    RV32_ERR_ELF_FILE,
+    RV32_ERR_MALLOC,
+}rv32_err_t;
+
+#define RV32_ASSERT(_x) if(_x != RV32_SUCCESS) return _x
+#define RV32_ASSERT_GOTO(_x, _sec) if(_x != RV32_SUCCESS) goto _sec
